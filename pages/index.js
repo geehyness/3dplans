28dcc54c38431053e3a6a54a25a22b8914d1a331
geehyness <geehyness22@gmail.com>
2022-09-13 00:11:42 +0200

    added house

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
       </Head>
 
       <div className="globe">
-        <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+        <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 22 }}>
           <ambientLight intensity={0.7} />
           <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
           <Suspense fallback={null}>
@@ -33,7 +33,7 @@ export default function Home() {
             {/* To add environment effect to the model */}
             <Environment preset="city" />
           </Suspense>
-          <OrbitControls autoRotate />
+          <OrbitControls  />
         </Canvas>
       </div>
 
